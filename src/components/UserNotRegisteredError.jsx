d435878a5518f723261b05996ee7f89008ca60b3
santos77 <flaviosattos123@gmail.com>
2026-09-06 03:36:11 -0300

    Add UserNotRegisteredError component

---
+++ src/components/UserNotRegisteredError.jsx
@@ -0,0 +1,8 @@
+export default function UserNotRegisteredError() {
+  return (
+    <div>
+      <h2>User Not Registered</h2>
+      <p>You are not registered. Please contact support.</p>
+    </div>
+  );
+}
