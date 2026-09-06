749aa768e2b2138013120fea986b685a08b14c29
santos77 <flaviosattos123@gmail.com>
2026-09-06 04:27:33 -0300

    Refactor Toast component to use Radix UI primitives

--- src/components/ui/toast.jsx
+++ src/components/ui/toast.jsx
@@ -1,11 +1,24 @@
-export function Toast() {
-  return <div>Toast</div>;
-}
+import * as React from "react"
+import * as ToastPrimitives from "@radix-ui/react-toast"
 
-export function ToastProvider({ children }) {
-  return <>{children}</>;
-}
+export const ToastProvider = ToastPrimitives.Provider
 
-export function ToastViewport() {
-  return null;
-}
+export const ToastViewport = React.forwardRef(({ className, ...props }, ref) => (
+  <ToastPrimitives.Viewport ref={ref} className={className} {...props} />
+))
+ToastViewport.displayName = ToastPrimitives.Viewport.displayName
+
+export const Toast = React.forwardRef(({ className, ...props }, ref) => (
+  <ToastPrimitives.Root ref={ref} className={className} {...props} />
+))
+Toast.displayName = ToastPrimitives.Root.displayName
+
+export const ToastClose = React.forwardRef(({ className, ...props }, ref) => (
+  <ToastPrimitives.Close ref={ref} className={className} {...props} />
+))
+ToastClose.displayName = ToastPrimitives.Close.displayName
+
+export const ToastDescription = React.forwardRef(({ className, ...props }, ref) => (
+  <ToastPrimitives.Description ref={ref} className={className} {...props} />
+))
+ToastDescription.displayName = ToastPrimitives.Description.displayName
