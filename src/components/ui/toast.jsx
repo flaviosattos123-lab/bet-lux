f9396589947a181d9af5770bb64f6342e882932c
santos77 <flaviosattos123@gmail.com>
2026-09-06 04:05:15 -0300

    Add Toast component and related functions

---
+++ src/components/ui/toast.jsx
@@ -0,0 +1,11 @@
+export function Toast() {
+  return <div>Toast</div>;
+}
+
+export function ToastProvider({ children }) {
+  return <>{children}</>;
+}
+
+export function ToastViewport() {
+  return null;
+}
