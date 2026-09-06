3b8067c18abdc6a6a293f684a7b9921a51c51a5f
santos77 <flaviosattos123@gmail.com>
2026-09-05 23:03:37 -0300

    Add initial App component with welcome message

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px', fontFamily: 'sans-serif' }}>
+      <h1>Bet Lux</h1>
+      <p>Projeto rodando com sucesso na Vercel!</p>
+    </div>
+  )
+}
