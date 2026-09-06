29386af55a93d86d6a91d52fcbc4c686d7f4cbfe
santos77 <flaviosattos123@gmail.com>
2026-09-06 02:18:39 -0300

    Remove PageNotFound route from App component
    Removed PageNotFound route from the application.

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { Toaster } from "@/components/ui/toaster"
 import { QueryClientProvider } from '@tanstack/react-query'
 import { queryClientInstance } from '@/lib/query-client'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import PageNotFound from './lib/PageNotFound';
+
 import { AuthProvider, useAuth } from '@/lib/AuthContext';
 import UserNotRegisteredError from '@/components/UserNotRegisteredError';
 import ScrollToTop from './components/ScrollToTop';
@@ -51,7 +51,7 @@ const AuthenticatedApp = () => {
       </Route>
       <Route path="/forgot-password" element={<ForgotPassword />} />
       <Route path="/reset-password" element={<ResetPassword />} />
-      <Route path="*" element={<PageNotFound />} />
+      
     </Routes>
   );
 };
